431310cbf4785a292e123ecaeebdd91d54fb4ba9
Egon Schmid <eschmid1972@gmail.com>
2025-09-22 14:30:22 +0200

    using sass-middleware for compiling scss files

--- server.js
+++ server.js
@@ -1,21 +1,41 @@
-require('./build.js')();
-
 const express = require('express');
 const app = express();
+const path = require('path');
+const sassMiddleware = require("sass-middleware");
 
 global.serverRoot = __dirname;
 
 const defaultPort = 3000;
 let port = process.argv[2] || defaultPort;
 
+const DEV = process.env.NODE_ENV !== "production";
+
+
+const SRC = path.join(__dirname, "src", "scss");
+const DEST = path.join(__dirname, "public", "css");
+
 /* nodemon workaround */
-if(port === 'server.js') {
+if (port === 'server.js') {
 	port = defaultPort;
 }
 
+app.use(
+	'/css',
+	sassMiddleware({
+		src: SRC,
+		dest: DEST,
+		prefix: "/css",
+		force: DEV,
+		sourceMap: DEV,
+		outputStyle: DEV ? "expanded" : "compressed"
+	})
+);
+
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
+
+app.use("/css", express.static(DEST, { maxAge: DEV ? 0 : "7d" }));
 
 // include body-parser
 const bodyParser = require('body-parser');
